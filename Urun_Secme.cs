2f6f7b6fec531d5e7b7920c444fa0ea541ab21d8
ismailvarli <ismailvarli1907@gmail.com>
2024-10-01 12:02:36 +0300

    Add files via upload

---
+++ Urun_Secme.cs
@@ -0,0 +1,15 @@
+﻿
+namespace c__kursu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urunler.UrunleriListele();
+            Urunler.Urunsecme();
+            Urunler.UcuncuUrun();
+        }   
+    }
+
+}
